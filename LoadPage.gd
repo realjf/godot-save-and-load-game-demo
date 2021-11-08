12248f4e0b2680c2fc7303d144cb44da03c85990
realjf <real_jf@hotmail.com>
2021-11-08 16:46:26 +0800

    update

--- LoadPage.gd
+++ LoadPage.gd
@@ -3,13 +3,21 @@ extends Control
 
 onready var vbox = $ScrollContainer/VBoxContainer
 
+var selected: RichTextLabel = null
 
 
 func _on_ExitButton_pressed():
 	queue_free()
-
-
+	
 func _process(delta):
+	var records = vbox.get_children()
+	for r in records:
+		var n = r.get_node("RichTextLabel").has_focus()
+		if n:
+			selected = r.get_node("RichTextLabel")
+
+
+func _on_LoadButton_pressed():
+	print("loading: " + selected.bbcode_text)
 	pass
-
-
+	
